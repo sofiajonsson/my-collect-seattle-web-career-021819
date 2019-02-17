83b79f275214d778f095bf08256df6bdeef875fc
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-17 05:47:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -36,5 +36,18 @@
 # array2
 # end
 
+def my_collect(array)
+  i = 0
+  result = []
+  while i < array.length
+    result.push(yield array[i])
+      i += 1
+  end
+  result
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
     
