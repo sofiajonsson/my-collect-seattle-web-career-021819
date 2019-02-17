63b94d5266a7226885ea656a4487f8a73499f8df
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-17 05:45:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,34 +4,59 @@
 # collection = ['ruby', 'javascript', 'python', 'objective-c']
 # students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-# def my_collect(array)
+def my_collect(array)
+#   new_arr = [] 
   
+#   i=0 
+#   while i < array.length 
+#   new_arr.push yield(array[i])
+#   i += 1 
+# end 
+#   # for element in array 
+#   #   new_arr.push yield element 
+#   # end 
+  
+#   new_arr
 # end 
 
 
-def my_collect(array) # put argument(s) here
-  if block_given?
-    i = 0
- 
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
- 
-    array
-  else
-    "Hey! No block was given!"
-  end
-end
+# my_collect(student) do |name| 
+#   name.split(" ").first 
+# end 
 
-def my_collect(array)
-  array2 = []
-  array3= []
-array.collect do |lang|
-  array2 << lang.upcase
-end
-array2
-end
+# my_collect(collection) do |lang|
+#   lang.upcase 
+# end 
+
+
+
+
+
+
+
+# def my_collect(array) # put argument(s) here
+#   if block_given?
+#     i = 0
+ 
+#     while i < array.length
+#       yield(array[i])
+#       i = i + 1
+#     end
+ 
+#     array
+#   else
+#     "Hey! No block was given!"
+#   end
+# end
+
+# def my_collect(array)
+#   array2 = []
+#   array3= []
+# array.collect do |lang|
+#   array2 << lang.upcase
+# end
+# array2
+# end
 
 
     
