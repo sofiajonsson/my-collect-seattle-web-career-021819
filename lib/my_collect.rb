49a4d02c2e24f59f230763a20e0deb8fd077ad34
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-17 05:46:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -12,29 +12,53 @@
 
 
 
-def my_collect(array) # put argument(s) here
-  if block_given?
-    i = 0
+# def my_collect(array) # put argument(s) here
+#   if block_given?
+#     i = 0
  
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
+#     while i < array.length
+#       yield(array[i])
+#       i = i + 1
+#     end
  
-    array
-  else
-    "Hey! No block was given!"
+#     array
+#   else
+#     "Hey! No block was given!"
+#   end
+# end
+
+# def my_collect(array)
+#   array2 = []
+#   array3= []
+# array.collect do |lang|
+#   array2 << lang.upcase
+# end
+# array2
+# end
+
+def my_each(array)
+  i = 0
+  while i < array.length
+# The "while" method will always return a value of => nil"
+    yield(array[i])
+    i = i + 1
   end
 end
 
-def my_collect(array)
-  array2 = []
-  array3= []
-array.collect do |lang|
-  array2 << lang.upcase
-end
-array2
-end
+my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
 
+# Use the collection method 
+# To make the *return-value* the answer you want
+# instead of "=> nil"
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
     
