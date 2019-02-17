1834bddc24d53ad205ca02fb56b0c2f7f458d852
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-17 05:49:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,21 @@
+def my_collect(array)
+  i = 0
+  result = []
+  while i < array.length
+    result.push(yield array[i])
+      i += 1
+  end
+  result
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+#--------- bottom attempt was 6/7 but so different....
 
 # collection = ['ruby', 'javascript', 'python', 'objective-c']
 # students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
-
-
-
-
-
-
 
 
 # def my_collect(array) # put argument(s) here
@@ -29,25 +35,12 @@
 
 # def my_collect(array)
 #   array2 = []
-#   array3= []
 # array.collect do |lang|
 #   array2 << lang.upcase
 # end
 # array2
 # end
 
-def my_collect(array)
-  i = 0
-  result = []
-  while i < array.length
-    result.push(yield array[i])
-      i += 1
-  end
-  result
-end
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
+
     
