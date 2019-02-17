6bf6f3c706623580bb21d0ca1d452e2a1ad484dc
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-17 05:46:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,59 +4,37 @@
 # collection = ['ruby', 'javascript', 'python', 'objective-c']
 # students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+
+
+
+
+
+
+
+
+def my_collect(array) # put argument(s) here
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    "Hey! No block was given!"
+  end
+end
+
 def my_collect(array)
-  new_arr = [] 
-  
-  i=0 
-  while i < array.length 
-  new_arr.push yield(array[i])
-  i += 1 
-end 
-  # for element in array 
-  #   new_arr.push yield element 
-  # end 
-  
-  new_arr
-end 
-
-
-my_collect(student) do |name| 
-  name.split(" ").first 
-end 
-
-my_collect(collection) do |lang|
-  lang.upcase 
-end 
-
-
-
-
-
-
-
-# def my_collect(array) # put argument(s) here
-#   if block_given?
-#     i = 0
- 
-#     while i < array.length
-#       yield(array[i])
-#       i = i + 1
-#     end
- 
-#     array
-#   else
-#     "Hey! No block was given!"
-#   end
-# end
-
-# def my_collect(array)
-#   array2 = []
-#   array3= []
-# array.collect do |lang|
-#   array2 << lang.upcase
-# end
-# array2
-# end
+  array2 = []
+  array3= []
+array.collect do |lang|
+  array2 << lang.upcase
+end
+array2
+end
 
 
     
