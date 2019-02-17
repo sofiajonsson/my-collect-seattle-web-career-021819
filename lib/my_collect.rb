6c074aa69206f172e08b8f679883454235c26415
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-17 05:45:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,28 +5,28 @@
 # students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
 def my_collect(array)
-#   new_arr = [] 
+  new_arr = [] 
   
-#   i=0 
-#   while i < array.length 
-#   new_arr.push yield(array[i])
-#   i += 1 
-# end 
-#   # for element in array 
-#   #   new_arr.push yield element 
-#   # end 
+  i=0 
+  while i < array.length 
+  new_arr.push yield(array[i])
+  i += 1 
+end 
+  # for element in array 
+  #   new_arr.push yield element 
+  # end 
   
-#   new_arr
-# end 
+  new_arr
+end 
 
 
-# my_collect(student) do |name| 
-#   name.split(" ").first 
-# end 
+my_collect(student) do |name| 
+  name.split(" ").first 
+end 
 
-# my_collect(collection) do |lang|
-#   lang.upcase 
-# end 
+my_collect(collection) do |lang|
+  lang.upcase 
+end 
 
 
 
